71fa42440d656b0c5af84bf1d7a9252db3a6e0f5
Alberto Berroteran <aberroteran@live.com>
2014-03-27 12:53:34 -0430

    Documentation and format

--- Generators/Generators.php
+++ Generators/Generators.php
@@ -3,41 +3,69 @@ namespace KTemplate\Generators;
 use KTemplate\Generate;
 
 abstract class Generators{
-	protected $stack = array();
-	protected $gen   = null; 
+    protected $stack = array();
+    protected $gen   = null; 
 
-	function __construct(Array $stack, Generate $generator){
-		$this->stack = $stack;
-		$this->gen   = $generator;
-	}
+    function __construct(Array $stack, Generate $generator){
+        $this->stack = $stack;
+        $this->gen   = $generator;
+    }
 
-	function nested($val){
-		$this->gen->addNested($val);
-	}
+    /**
+     * Init of nested block
+     * @param int $val typee of block
+     */
+    function nested($val){
+        $this->gen->addNested($val);
+    }
 
-	function token(){
-		$val = array_shift($this->stack);
-		if(is_null($val)){
-			$this->exception('No more tokens!');
-		}
-		return  $val;
-	}
+    /**
+     * Get the next token
+     * @return Token
+     */
+    function next(){
+        $val = array_shift($this->stack);
+        if(is_null($val)){
+            $this->exception('No more tokens!');
+        }
+        return  $val;
+    }
 
-	function nextAndRequire($val){
-		$token = $this->token();
-		if(!$token->is($val)){
-			$this->exception('Expenting '. $val);
-		}
-		return $token;
-	}
+    /**
+     * Get the next token and verifique the type
+     * @return Token
+     */
+    function nextRequire($val){
+        $token = $this->token();
+        if(!$token->is($val)){
+            $this->exception('Expenting '. $val);
+        }
+        return $token;
+    }
 
-	function exception($str){
-		throw $this->gen->parseError($str);
-	}
+    /**
+     * Mark the end of sentence
+     */
+    function end(){
+        if(!empty($this->stack)){
+            $this->exception('Un xpenting mode tokens');
+        }
+    }
 
-	function nl($str){
-		$this->gen->nl($str);
-	}
+    /**
+     * Thow Parse Exception with message $str
+     * @param string $str
+     */
+    function exception($str){
+        throw $this->gen->parseError($str);
+    }
 
-	abstract function generate();
+    /**
+     * Write new line with identation
+     */
+    function nl($str){
+        $this->gen->nl($str);
+    }
+
+    abstract function generate();
 }
