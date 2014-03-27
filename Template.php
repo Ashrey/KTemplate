784d864ce3eaaf4ac1df2d397a9db04d45917275
Alberto Berroteran <aberroteran@live.com>
2014-03-27 12:33:47 -0430

    Reindexed

--- Template.php
+++ Template.php
@@ -2,28 +2,28 @@
 namespace KTemplate;
 
 class Template{
-	protected static $config;
+    protected static $config;
 
-	static function configure($c){
-		self::$config = $c;
-	}
+    static function configure($c){
+        self::$config = $c;
+    }
 
-	static function load($name, $var){
-		$tpl      = self::$config['template_dir']. "/$name";
-		$id = hash('sha256', $tpl);
-		$compile  = self::$config['cache_dir'] . "/$id.php";
-		self::generate($tpl, $id, $var, $compile);
-		include $compile;
-		$fun = "_$id";
-		$fun($var);
-	}
+    static function load($name, $var){
+        $tpl      = self::$config['template_dir']. "/$name";
+        $id = hash('sha256', $tpl);
+        $compile  = self::$config['cache_dir'] . "/$id.php";
+        self::generate($tpl, $id, $var, $compile);
+        include $compile;
+        $fun = "_$id";
+        $fun($var);
+    }
 
-	static function generate($file, $id, $var, $compile){
-		if (!is_file($compile) || (filemtime($file) > filemtime($compile))){
-			$parse  = new Parse($file, $var);
-			$gen = new Generate($parse->getNodes(), $id, $var);
-			$buffer = $gen->generate();
-			file_put_contents($compile, $buffer);
-		}
-	}
+    static function generate($file, $id, $var, $compile){
+        if (!is_file($compile) || (filemtime($file) > filemtime($compile))){
+            $parse  = new Parse($file, $var);
+            $gen = new Generate($parse->getNodes(), $id, $var);
+            $buffer = $gen->generate();
+            file_put_contents($compile, $buffer);
+        }
+    }
 }
