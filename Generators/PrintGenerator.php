808c8d2163c35505c9e0f8f0a1addf9d13773059
Alberto Berroteran <aberroteran@live.com>
2014-03-30 14:08:21 -0430

    Add string print support

--- Generators/PrintGenerator.php
+++ Generators/PrintGenerator.php
@@ -5,19 +5,10 @@ use KTemplate\Filter\Filter;
 class PrintGenerator extends Generators{
 
     function generate(){
-        $token = $this->nextRequire(Token::T_STREAM, Token::T_IDENT);
+        $token = $this->nextRequire(Token::T_STREAM, Token::T_IDENT, Token::T_STRING );
         $str = $this->getPrint($token);
         $this->nl("echo $str ;\n");
         $this->end();
-    }
-
-    function getStream($token){
-        return "'".  addcslashes($token->getValue(), "'\\") . "'";
-    }
-
-    function getIdem($token){
-        $str = '$'.$token->getValue();
-        return $this->applyFilter($str);
     }
 
     function getPrint($token){
@@ -27,6 +18,37 @@ class PrintGenerator extends Generators{
                 break;
             case Token::T_IDENT :
                 return $this->getIdem($token);
+            case Token::T_STRING :
+                return $this->getStr($token);
         }
+    }
+
+    /**
+     * Get a Stream
+     * @param Token $token 
+     * @return string
+     */
+    function getStream($token){
+        return "'".  addcslashes($token->getValue(), "'\\") . "'";
+    }
+
+    /**
+     * Get a identifiquer
+     * @param Token $token 
+     * @return string
+     */
+    function getIdem($token){
+        $str = '$'.$token->getValue();
+        return $this->applyFilter($str);
+    }
+
+    /**
+     * Get a string
+     * @param Token $token 
+     * @return string
+     */
+    function getStr($token){
+        $str = $token->getValue();
+        return $this->applyFilter($str);
     }
 }
