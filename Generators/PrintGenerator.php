a03e38e3cb0e0ad2bce431421aceebef700f020c
Alberto Berroteran <aberroteran@live.com>
2014-05-07 14:09:54 -0430

    Implement toString

--- Generators/PrintGenerator.php
+++ Generators/PrintGenerator.php
@@ -6,48 +6,7 @@ class PrintGenerator extends Generators{
 
     function generate(){
         $token = $this->nextRequire(Token::T_STREAM, Token::T_IDENT, Token::T_STRING );
-        $str = $this->getPrint($token);
-        $this->output->write($str);
+        $this->output->write($this->applyFilter($token));
         $this->end();
-    }
-
-    function getPrint($token){
-        switch ($token->getType()) {
-            case Token::T_STREAM:
-                  return $this->getStream($token);
-            case Token::T_IDENT :
-                return $this->getIdem($token);
-            case Token::T_STRING :
-                return $this->getStr($token);
-        }
-    }
-
-    /**
-     * Get a Stream
-     * @param Token $token 
-     * @return string
-     */
-    function getStream($token){
-        return "'".  addcslashes($token->getValue(), "'\\") . "'";
-    }
-
-    /**
-     * Get a identifiquer
-     * @param Token $token 
-     * @return string
-     */
-    function getIdem($token){
-        $str = '$'.$token->getValue();
-        return $this->applyFilter($str);
-    }
-
-    /**
-     * Get a string
-     * @param Token $token 
-     * @return string
-     */
-    function getStr($token){
-        $str = $token->getValue();
-        return $this->applyFilter($str);
     }
 }
--- Token.php
+++ Token.php
@@ -8,20 +8,30 @@ class Token{
     const T_ALPHANUM   = 3;
     const T_STREAM     = 4;
     const T_IDENT      = 5;
+    const T_NULL       = 6;
+    const T_TRUE       = 7;
+    const T_FALSE      = 8;
 
     const T_PRINT      = 16;
     const T_IF         = 17;
     const T_ELSE       = 18;
-    const T_ELSEIF     = 19;
+    const T_ELIF       = 19;
     const T_ENDIF      = 20;
 
     const T_FOR        = 21;
     const T_ENDFOR     = 22;
     const T_IN         = 23;
 
+    const T_NOT        = 24;
+    const T_AND        = 25;
+    const T_OR         = 26;
+
+
+
     const T_COMMA      = 44;  /* , */
     const T_DOT        = 46;  /* . */
     const T_DDOT       = 58;  /* : */
+    const T_EQUAL      = 61;  /* = */
     const T_O_CORCH    = 91;  /* [ */
     const T_C_CORCH    = 93;  /* ] */
     const T_PIPE       = 124; /* | */
@@ -33,15 +43,20 @@ class Token{
         self::T_COMMENT  => 'comment',
         self::T_STREAM   => 'stream',
         self::T_NUMBER   => 'number',
-        self::T_STRING   => 'str',
+        self::T_STRING   => 'string',
         self::T_ALPHANUM => 'alphanum',
         self::T_FOR      => 'for',
         self::T_ENDFOR   => 'endfor',
         self::T_IN       => 'in',
         self::T_IF       => 'if',
         self::T_ELSE     => 'else',
-        self::T_ELSEIF   => 'elseif',
+        self::T_ELIF     => 'elif',
+        self::T_ENDIF    => 'endif',
         self::T_PIPE     => 'pipe',
+        self::T_AND      => 'and',
+        self::T_OR       => 'or',
+        self::T_NOT      => 'not',
+        self::T_EQUAL    => 'equal',
     );
     
     protected $value;
@@ -103,5 +118,44 @@ class Token{
      */
     function name(){
         return Token::$ALL_TOKEN[$this->type];
+    }
+
+
+    /**
+     * Print a identify
+     * @return string
+     */
+    function printIdent(){
+        return "\${$this->value}";
+    }
+
+    /**
+     * Print a equal
+     * @return string
+     */
+    function printEqual(){
+        return ' == ';
+    }
+
+    /**
+     * Print a steam
+     * @return string
+     */
+    function printStream(){
+        return "'".  addcslashes($this->value, "'\\") . "'";
+    }
+
+    /**
+     * Print a string
+     * @return string
+     */
+    function printString(){
+        return $this->value;
+    }
+
+
+    function __toString(){
+        $call = 'print'. $this->name();
+        return $this->$call();
     }
 }
