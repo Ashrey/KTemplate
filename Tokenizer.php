b1e35a418601bdba09ce27feb5f67c396b088302
Alberto Berroteran <aberroteran@live.com>
2014-05-18 14:07:23 -0430

    Remove string quote

--- Tokenizer.php
+++ Tokenizer.php
@@ -38,34 +38,6 @@ class Tokenizer{
         return false;
     }
 
-    /**
-     * Return token if next token is in list
-     * @return Token
-     */
-    function nextIs(){
-        $token = $this->next();
-        /*Not has token*/
-        if(!$token)
-            return false;
-        if($token->in(func_get_args())){
-            return $token;
-        }
-        $this->pos -= $token->length() + 1;
-        return  null;
-    }
-
-    /**
-     * Return token if next token is in list and thow exception if not more token
-     * @return Token
-     */
-    function nextIsRequired(){
-        $val = call_user_func_array(array($this, 'nextIs'), func_get_args());
-        if(!$val){
-            throw new ParseException($this->node, 'Expecting');
-        }
-        return  $val;
-    }
-
 
     /**
      * Return True if has token
@@ -76,18 +48,20 @@ class Tokenizer{
     }
 
     function str($start){
-        $offset = $this->pos+1;
+        $this->pos++;
+        $offset = $this->pos;
         do{
             $pos = strpos($this->str, $start, $offset);
             $offset = $pos+1;
         }while($pos && ($this->str[$pos-1] == '\\'));
-        if(!$pos){
+        if($pos === FALSE){
             throw new ParseException($this->node, 'unclosed string');  
         }
+
         /*calculate lenght of str end - init*/
-        $lenght = $pos - $this->pos + 1;
+        $lenght = $pos - $this->pos;
         $buffer = substr($this->str, $this->pos,  $lenght);
-        $this->pos = $pos+1;
+        $this->pos = $pos  + 1;
         return new Token(Token::T_STRING, $buffer);
     }
 
@@ -125,18 +99,8 @@ class Tokenizer{
         $key = array_search($word, Token::$ALL_TOKEN);  
         if($key !== FALSE){
             return new Token($key, $word);
-        }
-    
-        if($this->nextIs(Token::T_O_CORCH)){
-            $word.= '['; /*añado el corchete*/
-            $next = $this->nextIsRequired(Token::T_IDENT, Token::T_STRING, Token::T_NUMBER);
-            if($next->is(Token::T_IDENT)) $word.= '$';
-            $word.= $next->getValue();
-            $this->nextIsRequired(Token::T_C_CORCH);
-            return  new Token(Token::T_IDENT, "$word]");
-        }else{  
-            return  new Token(Token::T_IDENT, $word);
-        }
+        } 
+        return  new Token(Token::T_IDENT, $word);
     }
 
     /**
