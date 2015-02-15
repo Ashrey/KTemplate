0f4f241ff549eeec37920ecd274f2c06c1f9fc3d
Alberto Berroteran <aberroteran@live.com>
2015-02-15 18:12:44 -0430

    Add extends

--- Compiler.php
+++ Compiler.php
@@ -18,10 +18,40 @@ class Compiler {
 
 	protected $block = array();
 
-	function __construct() {
+	/**
+	 * Current block
+	 * @var string
+	 */
+	protected $current = 'main';
 
+	protected $file = '';
+
+	protected $compilated = '';
+
+	protected $parent = NULL;
+
+	protected $tpl = NULL;
+
+	protected $id = NULL;
+
+	function __construct(Template $tpl, $file) {
+		$this->tpl = $tpl;
+		$this->file = $tpl->resolverFileName($file);
 	}
 
+	function generate() {
+		$a = file($this->file);
+		$this->block[$this->current] = '';
+		$this->line = 0;
+		foreach ($a as $line) {
+			$this->line++;
+			$this->block[$this->current] .= preg_replace_callback(static::INTERPOLATE, array($this, 'callback'), $line);
+		}
+	}
+
+	function getBlocks() {
+		return $this->block;
+	}
 	function addNested($val) {
 		$this->nested[] = $val;
 	}
@@ -31,22 +61,6 @@ class Compiler {
 		if ($last != $val) {
 			throw $this->parseError("You closed $val and last opened was $last");
 		}
-	}
-
-	function parse($file) {
-		$a = file($file);
-		$str = '';
-		$this->line = 0;
-		foreach ($a as $line) {
-			$this->line++;
-			$str .= preg_replace_callback(static::INTERPOLATE, array($this, 'callback'), $line);
-		}
-		file_put_contents('tmp/exec.php', $str);
-	}
-
-	function go($var) {
-		extract($var);
-		include 'tmp/exec.php';
 	}
 
 	function callback($match) {
@@ -69,9 +83,9 @@ class Compiler {
 		$list = explode('|', $val);
 		$var = trim(array_shift($list));
 		if ($var[0] == '\'') {
-			return "echo $var";
+			return "<?php echo $var ?>";
 		}
-		return "echo \$$var";
+		return "<?php echo \$$var ?>";
 	}
 
 	/**
@@ -80,5 +94,30 @@ class Compiler {
 	 */
 	function parseError($str) {
 		return new ParseException($this->line, $str);
+	}
+
+	function getTpl() {
+		return $this->tpl;
+	}
+
+	function getFile() {
+		return $this->file;
+	}
+
+	function setCurrent($c) {
+		if (isset($this->block[$c])) {
+			$this->parseError("Can't rewrite block $c");
+		}
+		$this->block[$c] = '';
+		$this->current = $c;
+
+	}
+
+	function getParent() {
+		return $this->parent;
+	}
+
+	function setParent($name) {
+		$this->parent = $name;
 	}
 }
