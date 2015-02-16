69822d64a61ff112fa1fd00f57f0fc8f50cf2995
Alberto Berroteran <aberroteran@live.com>
2015-02-15 20:28:16 -0430

    add array support

--- Generators/PrintGenerator.php
+++ Generators/PrintGenerator.php
@@ -6,6 +6,11 @@ class PrintGenerator extends Generators{
 
     function generate(){
         $token = $this->nextRequire(Token::T_STREAM, Token::T_IDENT, Token::T_STRING );
+        if($this->nextIfIs(Token::T_O_CORCH)){
+            $next = $this->nextRequire(Token::T_IDENT, Token::T_STRING, Token::T_NUMBER);
+            $this->nextRequire(Token::T_C_CORCH);
+            $token = "{$token}[$next]";
+        }
         return $this->applyFilter($token);
     }
 }
